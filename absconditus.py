8c416e8af54d13ebedaaed9bdc26444b95356d9a
D4no <dano@controlhub.space>
2023-12-20 21:52:55 -0600

    Changes

--- absconditus.py
+++ absconditus.py
@@ -1,111 +1,260 @@
 import subprocess
 import sys
+import os
+import mimetypes
+from colorama import Fore, Style
+import time  # Added for the animation
 
-# Intentar importar colorama, instalarlo si no está disponible
+# Función para instalar e importar un paquete de Python
+def install_and_import(paquete):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", paquete])
+    __import__(paquete)
+
+# Lista de paquetes requeridos
+paquetes_requeridos = ["colorama", "cryptography"]
+
+# Instalar paquetes requeridos
+for paquete in paquetes_requeridos:
+    try:
+        __import__(paquete)
+    except ImportError:
+        print(f"Instalando {paquete}...")
+        install_and_import(paquete)
+
 try:
     import colorama
     from colorama import Fore, Style
-except ModuleNotFoundError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "colorama"])
-    import colorama
-    from colorama import Fore, Style
+except ImportError:
+    print("Colorama no está instalado y no se puede instalar automáticamente.")
+    sys.exit(1)
 
 colorama.init(autoreset=True)
 
-def box_text(text, padding=1):
-    lines = text.split('\n')
-    max_width = max(len(line) for line in lines)
-    box_width = max_width + 2 * padding
+def box_text(texto, padding=1):
+    líneas = texto.split('\n')
+    max_ancho = max(len(línea) for línea in líneas)
+    ancho_caja = max_ancho + 2 * padding
 
-    top_bottom = '+' + '-' * box_width + '+'
-    padded_lines = [f"|{' ' * padding}{line}{' ' * (box_width - len(line))}|" for line in lines]
+    arriba_abajo = '+' + '-' * ancho_caja + '+'
+    líneas_rellenas = [f"|{' ' * padding}{línea}{' ' * (ancho_caja - len(línea))}|" for línea in líneas]
 
-    boxed_text = [top_bottom] + padded_lines + [top_bottom]
-    return '\n'.join(boxed_text)
+    texto_encajado = [arriba_abajo] + líneas_rellenas + [arriba_abajo]
+    return '\n'.join(texto_encajado)
 
-# Mensaje de bienvenida en arte ASCII de d4no :D
-welcome_message = """
+mensaje_bienvenida = r"""
        __                        ___ __         
  ___ _/ /  ___ _______  ___  ___/ (_) /___ _____
 / _ `/ _ \(_-</ __/ _ \/ _ \/ _  / / __/ // (_-<
 \_,_/_.__/___/\__/\___/_//_/\_,_/_/\__/\_,_/___/
-                        by d4no @danobt  
+                        por d4no @danobt  
 """
 
-# Enmarcar el mensaje de bienvenida
-boxed_welcome = box_text(welcome_message)
-print(boxed_welcome)
+caja_mensaje_bienvenida = box_text(mensaje_bienvenida)
+print(caja_mensaje_bienvenida)
 
-def vigenere_cipher_encrypt(text, keyword):
-    text = text.upper()
-    keyword = keyword.upper()
-    keyword_repeated = (keyword * (len(text) // len(keyword) + 1))[:len(text)]
-    encrypted_text = ''
-
-    for t, k in zip(text, keyword_repeated):
-        if t.isalpha():
-            shift = ord(k) - ord('A')
-            encrypted_char = chr((ord(t) - ord('A') + shift) % 26 + ord('A'))
-            encrypted_text += encrypted_char
+# Add a simple text-based animation
+def simple_animation():
+    for _ in range(5):
+        os.system("cls" if os.name == "nt" else "clear")  # Clear the terminal screen
+        caja_mensaje_bienvenida = box_text(mensaje_bienvenida)
+        print(caja_mensaje_bienvenida)
+        print(Fore.RED + "Animation Frame 1" + Style.RESET_ALL)
+        time.sleep(0.5)
+        os.system("cls" if os.name == "nt" else "clear")  # Clear the terminal screen
+        caja_mensaje_bienvenida = box_text(mensaje_bienvenida)
+        print(caja_mensaje_bienvenida)
+        print(Fore.GREEN + "Animation Frame 2" + Style.RESET_ALL)
+        time.sleep(0.5)
+# Implementa tus funciones de cifrado Vigenère aquí
+def vigenere_cipher_encrypt(texto, palabra_clave):
+    resultado = []
+    longitud_palabra_clave = len(palabra_clave)
+    for i, carácter in enumerate(texto):
+        if carácter.isalpha():
+            desplazamiento = ord(palabra_clave[i % longitud_palabra_clave].lower()) - ord('a')
+            if carácter.islower():
+                resultado.append(chr(((ord(carácter) - ord('a') + desplazamiento) % 26) + ord('a')))
+            else:
+                resultado.append(chr(((ord(carácter) - ord('A') + desplazamiento) % 26) + ord('A')))
         else:
-            encrypted_text += t
+            resultado.append(carácter)
+    return ''.join(resultado)
 
-    return encrypted_text
-
-def vigenere_cipher_decrypt(encrypted_text, keyword):
-    keyword = keyword.upper()
-    keyword_repeated = (keyword * (len(encrypted_text) // len(keyword) + 1))[:len(encrypted_text)]
-    decrypted_text = ''
-
-    for t, k in zip(encrypted_text, keyword_repeated):
-        if t.isalpha():
-            shift = ord(k) - ord('A')
-            decrypted_char = chr((ord(t) - ord('A') - shift) % 26 + ord('A'))
-            decrypted_text += decrypted_char
+def vigenere_cipher_decrypt(encrypted_text, palabra_clave):
+    resultado = []
+    longitud_palabra_clave = len(palabra_clave)
+    for i, carácter in enumerate(encrypted_text):
+        if carácter.isalpha():
+            desplazamiento = ord(palabra_clave[i % longitud_palabra_clave].lower()) - ord('a')
+            if carácter.islower():
+                resultado.append(chr(((ord(carácter) - ord('a') - desplazamiento + 26) % 26) + ord('a')))
+            else:
+                resultado.append(chr(((ord(carácter) - ord('A') - desplazamiento + 26) % 26) + ord('A')))
         else:
-            decrypted_text += t
+            resultado.append(carácter)
+    return ''.join(resultado)
 
-    return decrypted_text
+# Implementa métodos de cifrado adicionales aquí
+def caesar_cipher(texto, desplazamiento, cifrar=True):
+    resultado = []
+    for carácter in texto:
+        if carácter.isalpha():
+            if carácter.islower():
+                if cifrar:
+                    resultado.append(chr(((ord(carácter) - ord('a') + desplazamiento) % 26) + ord('a')))
+                else:
+                    resultado.append(chr(((ord(carácter) - ord('a') - desplazamiento + 26) % 26) + ord('a')))
+            else:
+                if cifrar:
+                    resultado.append(chr(((ord(carácter) - ord('A') + desplazamiento) % 26) + ord('A')))
+                else:
+                    resultado.append(chr(((ord(carácter) - ord('A') - desplazamiento + 26) % 26) + ord('A')))
+        else:
+            resultado.append(carácter)
+    return ''.join(resultado)
 
-def get_multiline_input(prompt):
+def reverse_cipher(texto, cifrar=True):
+    if cifrar:
+        return texto[::-1]  # Invertir el texto
+    else:
+        return texto[::-1]  # Invertir el texto (el descifrado es igual que el cifrado para el cifrado inverso)
+
+def guardar_a_archivo(contenido, nombre_archivo):
+    with open(nombre_archivo + ".txt", 'w') as archivo:
+        archivo.write(contenido)
+    print(Fore.GREEN + "Salida guardada en " + nombre_archivo + ".txt" + Style.RESET_ALL)
+
+def encrypt_decrypt(texto, método, palabra_clave=None, cifrar=True):
+    if método == 'vigenere':
+        return vigenere_cipher_encrypt(texto, palabra_clave) if cifrar else vigenere_cipher_decrypt(texto, palabra_clave)
+    elif método == 'caesar':
+        desplazamiento = int(input(Fore.YELLOW + "Ingrese el valor de desplazamiento: " + Style.RESET_ALL))
+        return caesar_cipher(texto, desplazamiento, cifrar)
+    elif método == 'reverse':
+        return reverse_cipher(texto, cifrar)
+    else:
+        raise ValueError(Fore.RED + "Método desconocido: " + método + Style.RESET_ALL)
+
+def obtener_entrada_multilínea(prompt):
     print(Fore.YELLOW + prompt + Style.RESET_ALL)
-    lines = []
+    líneas = []
     while True:
-        line = input()
-        if line:
-            lines.append(line)
+        línea = input()
+        if línea:
+            líneas.append(línea)
         else:
             break
-    return '\n'.join(lines)
+    return '\n'.join(líneas)
+def leer_desde_archivo(ruta_archivo):
+    with open(ruta_archivo, 'r') as archivo:
+        return archivo.read()
 
-def read_from_file(file_path):
-    with open(file_path, 'r') as file:
-        return file.read()
+# Función para cifrar un archivo
+def cifrar_archivo(ruta_archivo, ruta_salida, contraseña):
+    try:
+        # Leer el contenido del archivo
+        with open(ruta_archivo, 'rb') as archivo:
+            datos_archivo = archivo.read()
+
+        # Cifrar el contenido del archivo
+        suite_cifrado = Fernet(contraseña)
+        datos_cifrados = suite_cifrado.encrypt(datos_archivo)
+
+        # Escribir el contenido cifrado en un nuevo archivo
+        with open(ruta_salida, 'wb') as archivo_cifrado:
+            archivo_cifrado.write(datos_cifrados)
+
+        print(Fore.GREEN + "Archivo cifrado con éxito: " + ruta_salida + Style.RESET_ALL)
+    except Exception as e:
+        print(Fore.RED + "Error al cifrar el archivo: " + str(e) + Style.RESET_ALL)
+
+# Función para descifrar un archivo
+def descifrar_archivo(ruta_archivo, ruta_salida, contraseña):
+    try:
+        # Leer el contenido cifrado del archivo
+        with open(ruta_archivo, 'rb') as archivo_cifrado:
+            datos_cifrados = archivo_cifrado.read()
+
+        # Descifrar el contenido del archivo
+        suite_cifrado = Fernet(contraseña)
+        datos_descifrados = suite_cifrado.decrypt(datos_cifrados)
+
+        # Escribir el contenido descifrado en un nuevo archivo
+        with open(ruta_salida, 'wb') as archivo_descifrado:
+            archivo_descifrado.write(datos_descifrados)
+
+        print(Fore.GREEN + "Archivo descifrado con éxito: " + ruta_salida + Style.RESET_ALL)
+    except Exception as e:
+        print(Fore.RED + "Error al descifrar el archivo: " + str(e) + Style.RESET_ALL)
+
+def procesar_archivos():
+    print(Fore.GREEN + "Ingrese la operación - 1 para Cifrar, 2 para Descifrar:" + Style.RESET_ALL)
+    operación = int(input().strip())  # Convertir a entero
+
+    contraseña = input(Fore.YELLOW + "Ingrese la contraseña para cifrar/descifrar el archivo: " + Style.RESET_ALL)
+
+    if operación == 1:
+        # Cifrar archivo
+        ruta_archivo = input(Fore.YELLOW + "Ingrese la ruta del archivo para cifrar: " + Style.RESET_ALL)
+        ruta_salida = input(Fore.YELLOW + "Ingrese la ruta para guardar el archivo cifrado: " + Style.RESET_ALL)
+        cifrar_archivo(ruta_archivo, ruta_salida, contraseña)
+    elif operación == 2:
+        # Descifrar archivo
+        ruta_archivo = input(Fore.YELLOW + "Ingrese la ruta del archivo para descifrar: " + Style.RESET_ALL)
+        ruta_salida = input(Fore.YELLOW + "Ingrese la ruta para guardar el archivo descifrado: " + Style.RESET_ALL)
+        descifrar_archivo(ruta_archivo, ruta_salida, contraseña)
+    else:
+        print(Fore.RED + "Operación no válida seleccionada." + Style.RESET_ALL)
 
 def main():
-    print(Fore.GREEN + "Elige una operación - '1' para Cifrado, '2' para Descifrado:" + Style.RESET_ALL)
-    operation = input().strip()
-    print(Fore.GREEN + "Ingresa '1' para escribir el texto, '2' para leer desde un archivo:" + Style.RESET_ALL)
-    input_method = input().strip()
+    print(Fore.GREEN + "Elija una operación - 1 para Cifrar, 2 para Descifrar, 3 para Procesar Archivos:" + Style.RESET_ALL)
+    operación = int(input().strip())  # Convertir a entero
 
-    if input_method == '1':
-        text = get_multiline_input("Ingresa el texto (presiona Enter dos veces para terminar): ")
-    elif input_method == '2':
-        file_path = input(Fore.YELLOW + "Ingresa la ruta del archivo: " + Style.RESET_ALL)
-        text = read_from_file(file_path)
+    if operación == 1 or operación == 2:
+        # Cifrar o descifrar texto
+        método_input = input(Fore.GREEN + "Ingrese el método de cifrado: vigenere, caesar, reverse" + Style.RESET_ALL)
+        if método_input not in ['vigenere', 'caesar', 'reverse']:
+            raise ValueError(Fore.RED + "Método desconocido: " + método_input + Style.RESET_ALL)
+        método = método_input
+
+        texto = ''
+        if operación in [1, 2]:
+            print(Fore.GREEN + "Ingrese '1' para ingresar texto, '2' para leer desde un archivo:" + Style.RESET_ALL)
+            método_entrada = input().strip()
+
+            if método_entrada == '1':
+                texto = obtener_entrada_multilínea("Ingrese el texto (presione Enter dos veces para finalizar): ")
+            elif método_entrada == '2':
+                ruta_archivo = input(Fore.YELLOW + "Ingrese la ruta del archivo: " + Style.RESET_ALL)
+                texto = leer_desde_archivo(ruta_archivo)
+            else:
+                raise ValueError(Fore.RED + "Método de entrada no válido seleccionado." + Style.RESET_ALL)
+
+        palabra_clave = ''
+        if método == 'vigenere':
+            palabra_clave = input(Fore.YELLOW + "Ingrese la palabra clave: " + Style.RESET_ALL)
+
+        # Declaración de impresión de depuración
+        print(f"Depuración: Operación seleccionada es {operación}, Método seleccionado es '{método}'")
+
+        resultado = ''
+        if operación == 1:
+            resultado = encrypt_decrypt(texto, método, palabra_clave, True)
+        elif operación == 2:
+            resultado = encrypt_decrypt(texto, método, palabra_clave, False)
+        else:
+            raise ValueError(Fore.RED + "Operación no válida seleccionada." + Style.RESET_ALL)
+
+        print(Fore.MAGENTA + "\nResultado:\n" + resultado + Style.RESET_ALL)
+
+        if input(Fore.CYAN + "¿Desea guardar el resultado en un archivo? (s/n): " + Style.RESET_ALL).lower() == 's':
+            nombre_archivo = input(Fore.YELLOW + "Ingrese el nombre de archivo para guardar la salida: " + Style.RESET_ALL)
+            guardar_a_archivo(resultado, nombre_archivo)
+    elif operación == 3:
+        # Procesar archivos
+        procesar_archivos()
     else:
-        raise ValueError(Fore.RED + "Método de entrada inválido seleccionado." + Style.RESET_ALL)
-
-    keyword = input(Fore.YELLOW + "Ingresa la palabra clave: " + Style.RESET_ALL)
-
-    if operation == '1':
-        result = vigenere_cipher_encrypt(text, keyword)
-        print(Fore.MAGENTA + "\nMensaje Cifrado:\n" + result + Style.RESET_ALL)
-    elif operation == '2':
-        result = vigenere_cipher_decrypt(text, keyword)
-        print(Fore.MAGENTA + "\nMensaje Descifrado:\n" + result + Style.RESET_ALL)
-    else:
-        raise ValueError(Fore.RED + "Operación inválida seleccionada." + Style.RESET_ALL)
+        print(Fore.RED + "Operación no válida seleccionada." + Style.RESET_ALL)
 
 if __name__ == "__main__":
     main()
